fa65862fd78cfa31a38bc2032113ed9885d5237c
Kássia Catarine <kassia_catarine.15@hotmail.com>
2020-07-31 21:32:22 -0300

    adding initial log removal structure
    Signed-off-by: Kássia Catarine <kassia_catarine.15@hotmail.com>

--- ErrorCentral.API/v1/Controllers/LogErrorsController.cs
+++ ErrorCentral.API/v1/Controllers/LogErrorsController.cs
@@ -45,5 +45,25 @@ namespace ErrorCentral.API.v1.Controllers
 
             return Ok();
         }
+
+        [HttpDelete("{id:int}")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        public async Task<IActionResult> RemoveLogErrorAsync([FromRoute] int id)
+        {
+            _logger.LogInformation(
+                "----- Sending request: {ServiceName} - {ServiceMethod}: ({@Id})",
+                nameof(ILogErrorService),
+                "RemoveAsync",
+                id);
+
+            var result = await _logErrorService.RemoveAsync(id);
+            if (!result)
+            {
+                return BadRequest();
+            }
+
+            return Ok();
+        }
     }
 }
--- ErrorCentral.Application/Services/ILogErrorService.cs
+++ ErrorCentral.Application/Services/ILogErrorService.cs
@@ -7,5 +7,6 @@ namespace ErrorCentral.Application.Services
     public interface ILogErrorService
     {
         Task<bool> CreateAsync(CreateLogErrorViewModel model, CancellationToken cancellationToken = default);
+        Task<bool> RemoveAsync(int id);
     }
 }
