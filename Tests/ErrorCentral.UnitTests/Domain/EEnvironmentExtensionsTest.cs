9865464a7788ed5c9e73e485e4b43be4bc9c2c03
Roger Lenke <rogerlenke@gmail.com>
2020-08-03 17:15:58 -0300

    Excluding some files from tests, adding extension methods test

---
+++ Tests/ErrorCentral.UnitTests/Domain/EEnvironmentExtensionsTest.cs
@@ -0,0 +1,22 @@
+﻿using ErrorCentral.Domain.AggregatesModel.LogErrorAggregate;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ErrorCentral.UnitTests.Domain
+{
+    public class EEnvironmentExtensionsTest
+    {
+        [Theory]
+        [InlineData(EEnvironment.Development, "Development")]
+        [InlineData(EEnvironment.Homologation, "Homologation")]
+        [InlineData(EEnvironment.Production, "Production")]
+
+        public void TestToFriendlyString(EEnvironment environment, string text)
+        {
+            environment.ToFriendlyString().Should().Be(text);
+        }
+    }
+}
---
+++ Tests/ErrorCentral.UnitTests/Domain/ELevelExtensionsTest.cs
@@ -0,0 +1,22 @@
+﻿using ErrorCentral.Domain.AggregatesModel.LogErrorAggregate;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ErrorCentral.UnitTests.Domain
+{
+    public class ELevelExtensionsTest
+    {
+        [Theory]
+        [InlineData(ELevel.Debug, "Debug")]
+        [InlineData(ELevel.Error, "Error")]
+        [InlineData(ELevel.Warning, "Warning")]
+
+        public void TestToFriendlyString(ELevel environment, string text)
+        {
+            environment.ToFriendlyString().Should().Be(text);
+        }
+    }
+}
