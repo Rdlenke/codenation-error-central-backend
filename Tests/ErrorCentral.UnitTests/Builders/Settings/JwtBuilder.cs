99a46cba628c7e579c68e5a32669606c2e23c0ff
Kássia Catarine <kassia_catarine.15@hotmail.com>
2020-08-08 11:00:36 -0300

    testing token generation
    Signed-off-by: Kássia Catarine <kassia_catarine.15@hotmail.com>

---
+++ Tests/ErrorCentral.UnitTests/Builders/Settings/JwtBuilder.cs
@@ -0,0 +1,38 @@
+﻿using ErrorCentral.Application.Settings;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ErrorCentral.UnitTests.Builders.Settings
+{
+    public class JwtBuilder : IBuilder<Jwt>
+    {
+        private Jwt _jwt;
+        public string Secret => "js6%\\$(tzp8E(Lu!s4DhM.$ABNtj4Cqmf.:U";
+        public int Expiration => 2;
+        public string Issuer => "MyEnvironment";
+        public string Audience => "https://localhost";
+
+        public JwtBuilder()
+        {
+            _jwt = WithDefaultValues();
+        }
+
+        public Jwt WithDefaultValues()
+        {
+            _jwt = new Jwt()
+            {
+                Secret = Secret,
+                Expiration = Expiration,
+                Issuer = Issuer,
+                Audience = Audience,
+            };
+            return _jwt;
+        }
+
+        public Jwt Build()
+        {
+            return _jwt;
+        }
+    }
+}
