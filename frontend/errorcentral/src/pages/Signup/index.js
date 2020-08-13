881163eae47f05a1854f5589487d715684485159
Tamara Montijo <tamara.montijo@gmail.com>
2020-08-13 20:28:42 -0300

    frontend sign in and sign up

--- frontend/errorcentral/src/pages/Signup/index.js
+++ frontend/errorcentral/src/pages/Signup/index.js
@@ -1,7 +1,34 @@
-import React from 'react';
+import React from "react";
+import { Scope } from "@unform/core";
+import { Form } from "@unform/web";
+import Input from "../../components/Form/input";
+import "../../components/Form/styles.css";
+import logoImg from '../../assets/images/ErrorCentralLogo.png';
 
 const Signup = () => {
-  return <h1>Signup</h1>;
+  function handleSubmit(data, { reset }) {
+    console.log(data);
+
+    reset();
+  }
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <img
+        src={logoImg}
+        // height="150"
+        // width="175"
+        alt="logo"
+      />
+
+      <Input name="name" label="Nome" />
+      <Input name="name" label="Sobrenome" />
+      <Input name="email" label="E-mail" type="email" />
+      <Input name="password" label="Senha" type="password" />
+
+      <button type="submit">Cadastrar</button>
+    </Form>
+  );
 };
 
 export default Signup;
