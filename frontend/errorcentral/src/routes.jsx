517cbcf477f8c2d26447b17f10cccfb888b057d1
Kássia Catarine <kassia_catarine.15@hotmail.com>
2020-08-15 11:29:14 -0300

    Add material ui
    Signed-off-by: Kássia Catarine <kassia_catarine.15@hotmail.com>

--- frontend/errorcentral/src/routes.jsx
+++ frontend/errorcentral/src/routes.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Route, BrowserRouter } from 'react-router-dom';
 import Signin from './pages/Signin';
 import Signup from './pages/Signup';
+import ListErrors from './pages/ListErrors';
 
 const Routes = () => {
   return (
     <BrowserRouter>
-      <Route component={Signin} path='/' exact />
+      <Route component={ListErrors} path='/' exact />
+      <Route component={Signin} path='/login' exact />
       <Route component={Signup} path='/join' />
     </BrowserRouter>
   );
