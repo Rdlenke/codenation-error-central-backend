430700725a820dab93c6eddda8e56eb063ad6511
Kássia Catarine <kassia_catarine.15@hotmail.com>
2020-08-15 17:21:55 -0300

    Add details error component
    Signed-off-by: Kássia Catarine <kassia_catarine.15@hotmail.com>

--- frontend/errorcentral/src/components/ErrorItem/index.jsx
+++ frontend/errorcentral/src/components/ErrorItem/index.jsx
@@ -4,18 +4,17 @@ import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import ListItemText from '@material-ui/core/ListItemText';
 import ErrorIconItem from '../ErrorIconItem';
 import ErrorActionsItem from '../ErrorActionsItem';
+import { Link } from 'react-router-dom';
 
-const ErrorItem = () => {
-
-
+const ErrorItem = (props) => {
   return (
-    <ListItem button>
+    <ListItem button component={Link} to={`erros/${1}`}>
       <ListItemAvatar>
         <ErrorIconItem />
       </ListItemAvatar>
       <ListItemText
-        primary="Nome do erro"
-        secondary={'Secondary text'}
+        primary={props.title}
+        secondary={props.descripton}
       />
       <ErrorActionsItem />
     </ListItem>
---
+++ frontend/errorcentral/src/pages/DetailsError/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const DetailsError = () => {
+  return (
+    <h1>Ola</h1>
+  );
+}
+
+export default DetailsError;
