5a0ff8d9d30ccecb0d8149458be3b39cb0cd9119
Kássia Catarine <kassia_catarine.15@hotmail.com>
2020-08-15 21:21:45 -0300

    Add loading in list errors
    Signed-off-by: Kássia Catarine <kassia_catarine.15@hotmail.com>

--- frontend/errorcentral/src/pages/ListErrors/index.jsx
+++ frontend/errorcentral/src/pages/ListErrors/index.jsx
@@ -2,14 +2,29 @@ import React, { useState, useEffect } from 'react';
 
 import api from '../../services/api';
 
+import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-import List from '@material-ui/core/List';
-import ErrorItem from '../../components/ErrorItem';
 import Divider from '@material-ui/core/Divider';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import List from '@material-ui/core/List';
+
+import ErrorItem from '../../components/ErrorItem';
 import ListBar from '../../components/ListBar';
 import ListNotice from '../../components/ListNotice';
 
+const useStyles = makeStyles({
+  containerProgress: {
+    display: 'flex',
+    flexDirection: 'column',
+    width: '100%',
+    minHeight: '80vh',
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 const ListErrors = () => {
+  const classes = useStyles();
   const [errors, setErrors] = useState({ data: null, loading: false, status: 0 });
   useEffect(() => {
     setErrors({ data: null, loading: true, status: 0 });
@@ -19,7 +34,8 @@ const ListErrors = () => {
         console.log('error', error);
         setErrors({ loading: false, status: error.request.status });
       });
-  }, [])
+  }, []);
+
   return (
     <Container maxWidth="md">
       <ListBar lenght={errors.data ? errors.data.lenght : 0} />
@@ -34,7 +50,11 @@ const ListErrors = () => {
             )) : <ListNotice status={errors.status} />
           }
         </List>
-      ) : (<h1>Carregando...</h1>)}
+      ) : (
+        <div className={classes.containerProgress}>
+          <CircularProgress />
+        </div>
+      )}
     </Container>
   );
 }
