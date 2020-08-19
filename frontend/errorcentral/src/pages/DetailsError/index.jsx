80eba8feb4be45c96db6cb246fcd81d0eda46a43
Tamara Montijo <tamara.montijo@gmail.com>
2020-08-19 09:59:00 -0300

    page error details frontend

--- frontend/errorcentral/src/pages/DetailsError/index.jsx
+++ frontend/errorcentral/src/pages/DetailsError/index.jsx
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+
+import api from '../../services/api';
 
 import Errors from '../../mock/errors';
 import backIcon from '../../assets/icons/back.svg'
-import logoImg from '../../assets/images/ErrorCentralLogo.png';
+import bugImg from '../../assets/images/BugImage.png';
+import arquivarImg from '../../assets/images/Arquivar.png';
+
 
 import './styles.css';
 
 const DetailsError = () => {
+  const [errors, setErrors] = useState({ data: [], loading: false, status: 0 });
+  useEffect(() => {
+    setErrors({ data: [], loading: true, status: 0 });
+    api.get('v1/logerrors')
+      .then(response => {})
+      .catch(error => {
+        // setErrors({ data: null, loading: false, status: error.request.status });
+        setErrors({ data: Errors, loading: false, status: 0 });
+      });
+  }, []);
+
   return (
     <div id="page-details-error" className="container">
       <header className="page-header">
@@ -24,6 +39,32 @@ const DetailsError = () => {
           <strong>Detalhes do erro:</strong>
         </div>
       </header>
+
+      <main>
+        <article className="error-item">
+          <header>
+            <img src={bugImg} alt="error type"/>
+            <div>
+              <strong>Nome do erro</strong>
+              <span>http://localhost:3000</span>
+            </div>
+          </header>
+          <p>
+            Detalhes do erro: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at orci eget erat iaculis ultrices ut nec tortor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent ut leo ut lacus fermentum cur
+          </p>
+
+          <footer>
+            <p>
+              Identificador:
+              <strong>id</strong>
+            </p>
+            <button type="button">
+              <img src={arquivarImg} alt="arquivar"/>
+            </button>
+          </footer>
+        </article>
+      </main>
+
     </div>
   );
 }
