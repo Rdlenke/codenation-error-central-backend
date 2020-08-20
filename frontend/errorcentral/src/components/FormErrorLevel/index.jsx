7b2fc7190f47db8cb80548944aadbd7a206f59be
Kássia Catarine <kassia_catarine.15@hotmail.com>
2020-08-20 18:56:05 -0300

    Add form for filter level
    Signed-off-by: Kássia Catarine <kassia_catarine.15@hotmail.com>

---
+++ frontend/errorcentral/src/components/FormErrorLevel/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const FormErrorLevel = () => {
+  return (
+    <h1>Ola</h1>
+  );
+}
+
+export default FormErrorLevel;
--- frontend/errorcentral/src/pages/ListErrors/index.jsx
+++ frontend/errorcentral/src/pages/ListErrors/index.jsx
@@ -7,6 +7,7 @@ import Container from '@material-ui/core/Container';
 import Divider from '@material-ui/core/Divider';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import List from '@material-ui/core/List';
+import Grid from '@material-ui/core/Grid';
 
 import ErrorItem from '../../components/ErrorItem';
 import ListBar from '../../components/ListBar';
@@ -34,8 +35,9 @@ const ListErrors = () => {
   useEffect(() => {
     setErrors({ data: [], loading: true, status: 0 });
     api.get('v1/logerrors')
-      .then(response => {})
-      .catch(error => {
+      .then(response => {
+        setErrors({ data: response.data, loading: false, status: response.status });
+      }).catch(error => {
         // setErrors({ data: null, loading: false, status: error.request.status });
         setTimeout(function() {
           setErrors({ data: Errors, loading: false, status: 0 });
@@ -44,24 +46,34 @@ const ListErrors = () => {
   }, []);
 
   return (
-    <Container maxWidth="md">
+    <Container maxWidth="xl">
       <ListBar lenght={errors.data ? errors.data.length : 0} />
-      {!errors.loading ? (
-        <List component="nav" aria-label="errors">
-          {errors.data && errors.data.length > 0 ?
-            errors.data.map(error => (
-              <div key={error.id}>
-                <ErrorItem error={error} />
-                <Divider />
-              </div>
-            )) : <ListNotice status={errors.status} />
-          }
-        </List>
-      ) : (
-        <div className={classes.containerProgress}>
-          <CircularProgress />
-        </div>
-      )}
+      <Grid container spacing={2}>
+        <Grid item>
+          <h1>Ola</h1>
+        </Grid>
+        <Grid
+          item xs={12} sm container
+          direction="column"
+        >
+          {!errors.loading ? (
+            <List component="nav" aria-label="errors">
+              {errors.data && errors.data.length > 0 ?
+                errors.data.map(error => (
+                  <div key={error.id}>
+                    <ErrorItem error={error} />
+                    <Divider />
+                  </div>
+                )) : <ListNotice status={errors.status} />
+              }
+            </List>
+          ) : (
+            <div className={classes.containerProgress}>
+              <CircularProgress />
+            </div>
+          )}
+        </Grid>
+      </Grid>
     </Container>
   );
 }
