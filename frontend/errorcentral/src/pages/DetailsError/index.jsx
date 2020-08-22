145047348bb91c784b18bb3f75818daa42a5fcd4
Roger Lenke <rogerlenke@gmail.com>
2020-08-22 02:40:17 -0300

    Cleaning details page

--- frontend/errorcentral/src/pages/DetailsError/index.jsx
+++ frontend/errorcentral/src/pages/DetailsError/index.jsx
@@ -1,70 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import api from '../../services/api';
-import Grid from '@material-ui/core/Grid';
-import styled, { css } from "styled-components";
 import backIcon from '../../assets/icons/back.svg'
 import './styles.css';
-
-const Detalhes = styled.span`
-  font-family: Roboto;
-  font-style: normal;
-  font-weight: 400;
-  color: #121212;
-  font-size: 108px;
-`;
-
-const Rect = styled.div`
-  width: 211px;
-  height: 39px;
-  margin-left: 914px;
-  margin-top: 45px;
-`;
-
-const DetalhesRow = styled.div`
-  height: 130px;
-  flex-direction: row;
-  display: flex;
-  margin-top: 39px;
-  margin-left: 69px;
-  margin-right: 372px;
-`;
-
-const Rect2 = styled.div`
-  width: 122px;
-  height: 44px;
-  margin-left: 1403px;
-  font-family: Roboto;
-  font-style: normal;
-  font-weight: 400;
-  color: rgba(125,119,119,1);
-`;
-
-const Rect3 = styled.div`
-  width: 999px;
-  height: 518px;
-  font-family: Roboto;
-  font-style: normal;
-  font-weight: 400;
-  color: rgba(125,119,119,1);
-  font-size: 25px;
-`;
-
-const Rect4 = styled.div`
-  width: 231px;
-  height: 226px;
-  margin-left: 293px;
-  margin-top: 65px;
-`;
-
-const Rect3Row = styled.div`
-  height: 518px;
-  flex-direction: row;
-  display: flex;
-  margin-top: 25px;
-  margin-left: 69px;
-  margin-right: 305px;
-`;
 
 const DetailsError = (props) => {
   let { id } = props.params;
@@ -86,7 +24,7 @@ const DetailsError = (props) => {
   }, [id]);
 
   return (
-    <div id="page-details-error" className="container">
+<div id="page-details-error" className="container">
       <header className="page-header">
         <div className="top-bar-container">
           <Link to="/">
@@ -98,17 +36,9 @@ const DetailsError = (props) => {
         </div>
 
         <div className="header-content">
-          <strong>Erro no {errors.data.source}</strong>
+          <strong>Detalhes do erro:</strong>
         </div>
       </header>
-      <div>
-        <div className="detailsText">
-          <span>Detalhes:</span>
-        </div>
-        <div className="detailsContainer">
-          <span>{errors.data.details}</span>
-        </div>
-       </div>
     </div>
   );
 }
