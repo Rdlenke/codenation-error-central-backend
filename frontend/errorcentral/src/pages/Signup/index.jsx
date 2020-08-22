8638fbd4407da928cb86cb333d85c33b61ca464b
Kássia Catarine <kassia_catarine.15@hotmail.com>
2020-08-21 23:43:17 -0300

    changing fields for material components
    Signed-off-by: Kássia Catarine <kassia_catarine.15@hotmail.com>

--- frontend/errorcentral/src/pages/Signup/index.jsx
+++ frontend/errorcentral/src/pages/Signup/index.jsx
@@ -1,24 +1,75 @@
 import React, { useState } from "react";
 import api from '../../services/api';
-import { Form } from "@unform/web";
-import Input from "../../components/Form/input";
 import logoImg from '../../assets/images/ErrorCentralLogo.png';
-import './styles.css'
 import { connect } from 'react-redux';
 import { addUser } from '../../redux/actions/actionCreators';
 import { useHistory } from "react-router-dom";
 
+import { makeStyles } from '@material-ui/core/styles';
+import IconButton from '@material-ui/core/IconButton';
+import OutlinedInput from '@material-ui/core/OutlinedInput';
+import InputLabel from '@material-ui/core/InputLabel';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import FormControl from '@material-ui/core/FormControl';
+import TextField from '@material-ui/core/TextField';
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
+import Button from '@material-ui/core/Button';
+import Paper from '@material-ui/core/Paper';
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    minHeight: '90vh',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'center',
+    flexDirection: 'column'
+  },
+  marginBottom: {
+    marginBottom: theme.spacing(2),
+  },
+  form: {
+    display: 'flex',
+    width: '300px',
+    flexDirection: 'column',
+    padding: '20px 40px',
+  },
+  image: {
+    margin: '0 auto',
+    height: '150px',
+    minWidth: '100px',
+    maxWidth: '100%',
+  }
+}));
 
 const Signup = (props) => {
+  const classes = useStyles();
   const [state, setState] = useState({ data: [], loading: false, status: 0 });
+  const [values, setValues] = useState({
+    email: '',
+    password: '',
+    showPassword: false,
+  });
   const history = useHistory();
+
+
+  const handleChange = (prop) => (event) => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
+
+  const handleClickShowPassword = () => {
+    setValues({ ...values, showPassword: !values.showPassword });
+  };
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
 
   function handleSubmit(data, { reset }) {
     setState({data: [], loading: true, status: 0})
-
-    // props.addUser({email: "email", firstName: "name", lastName: "name", token: "token", guid: "guid"})
-    // history.push("/");
-
     api.post('v1/User/CreateUser', data)
       .then(response => {
 
@@ -37,27 +88,84 @@ const Signup = (props) => {
   }
 
   return (
-    <Form onSubmit={handleSubmit}>
-      <img
-        src={logoImg}
-        // height="150"
-        // width="175"
-        alt="logo"
-      />
-        <div className="form-container">
-          <Input name="FirstName" label="Nome" />
-          <Input name="LastName" label="Sobrenome" />
-          <Input name="Email" label="E-mail" type="email" />
-          <Input name="Password" label="Senha" type="password" />
+    <form className={classes.root} onSubmit={handleSubmit}>
+      <Paper className={classes.form}>
+        <img
+          src={logoImg}
+          alt="logo"
+          className={classes.image}
+        />
 
-          <button type="submit">Cadastrar</button>
-        </div>
-      <a href="/login">Login</a>
-    </Form>
+        <TextField
+          id="firstname"
+          name="firstname"
+          label="Nome"
+          placeholder="Ex: João"
+          type="text"
+          fullWidth
+          margin="normal"
+          variant="outlined"
+          className={classes.marginBottom}
+        />
+        <TextField
+          id="lastname"
+          name="lastname"
+          label="Sobrenome"
+          placeholder="Ex: Souza"
+          type="text"
+          fullWidth
+          margin="normal"
+          variant="outlined"
+          className={classes.marginBottom}
+        />
+        <TextField
+          id="email"
+          name="email"
+          label="E-mail"
+          placeholder="email@email.com"
+          type="email"
+          fullWidth
+          margin="normal"
+          variant="outlined"
+          className={classes.marginBottom}
+        />
+        <FormControl fullWidth variant="outlined" className={classes.marginBottom}>
+          <InputLabel htmlFor="password">Senha</InputLabel>
+          <OutlinedInput
+            id="password"
+            name="password"
+            type={values.showPassword ? 'text' : 'password'}
+            value={values.password}
+            onChange={handleChange('password')}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  edge="end"
+                >
+                  {values.showPassword ? <Visibility /> : <VisibilityOff />}
+                </IconButton>
+              </InputAdornment>
+            }
+            labelWidth={70}
+          />
+        </FormControl>
+        <Button
+          variant="contained"
+          color="primary"
+          type="submit"
+          className={classes.marginBottom}
+        >
+          Cadastrar
+        </Button>
+        <a href="/login" className={classes.marginBottom}>Login</a>
+      </Paper>
+    </form>
   );
 };
 
 const mapDispatchToProps = { addUser };
-
 
 export default connect(null, mapDispatchToProps)(Signup);
